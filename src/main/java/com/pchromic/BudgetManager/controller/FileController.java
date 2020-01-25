93ae9035c5b49c88a7226ca3b85cf20d79d40cc0
PChromic <pchromicz@gmail.com>
2020-01-25 18:22:47 +0100

    minor fixes in file service

--- src/main/java/com/pchromic/BudgetManager/controller/FileController.java
+++ src/main/java/com/pchromic/BudgetManager/controller/FileController.java
@@ -3,6 +3,8 @@ package com.pchromic.BudgetManager.controller;
 import com.pchromic.BudgetManager.service.FileService;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class FileController {
@@ -15,7 +17,9 @@ public class FileController {
 
     @GetMapping("/file/open")
     void openFile(@RequestParam(name = "path") String path) {
-        String filePath = "E://Projects/Angular/BudgetManager/src/assets/" + path;
+        //String filePath = "E://Projects/Angular/BudgetManager/src/assets/" + path;
+        String filePath = new File("").getAbsolutePath() + "\\" + path;
+        System.out.println(filePath);
         service.openFile(filePath);
     }
 
